b1a8578ee96a1d2ce3cab476f8c577a66cc6f2b5
hongfei <hongfei@dreamy.live>
2016-05-10 14:02:16 +0800

    添加图片上传

--- src/entrys/user.js
+++ src/entrys/user.js
@@ -1,9 +1,23 @@
-var PopupSign = require('../components/pop_up_sign');
+var Popup = require('../components/pop_up_sign');
+var cropper = require('../components/cropper.js');
 
-$(function(){
-	$('#avatar_mask').on('click',function(){
-		console.log('aaaa');
-	})
+$(function() {
+    $('#avatar_mask').on('click', function() {
+        var avatar = new Popup('#avatar_popup');
+        avatar.alert();
+
+        $('#avatar_upload').on('change', function() {
+            var val = $(this).val();
+            console.log(val);
+        })
+
+        $('#avatar_select').cropper({
+            aspectRatio: 1 / 1,
+            crop: function(e) {}
+        });
+
+
+    })
 
 
 
